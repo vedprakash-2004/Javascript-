2262c249706b897d5e3f76e5b4b39fb8ee538950
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-02 23:42:04 +0530

    conditional statement

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-let age = 18;
-console.log(age<18);
-
+//conditional statement
+console.log("before my if statement");
+let age = 23;
+if(age>=18){
+    console.log("you can vote");
+}
+console.log("after my if statement");
 
