5f7ec037d896d10a7e04a96944828285d3522a55
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-03 22:07:39 +0530

    practice problem solve

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-let size = "xl";
-if(size === "xl"){
-    console.log("price is rs. 250");
-}else if(size ==="l"){
-    console.log("price is rs. 200");
-}else if(size === "m"){
-    console.log("price is rs. 100");
+// let size = "xl";
+// if(size === "xl"){
+//     console.log("price is rs. 250");
+// }else if(size ==="l"){
+//     console.log("price is rs. 200");
+// }else if(size === "m"){
+//     console.log("price is rs. 100");
     
-}else{
-    console.log("price is rs. 50");
+// }else{
+//     console.log("price is rs. 50");
+// }
+
+let marks = 45;
+
+if(marks >= 33){
+    console.log("pass");
+}else if(marks >=50){
+    console.log("b+");
+
+}else if(marks >= 70){
+    console.log("A");
+}else if(marks >= 80){
+    console.log("A+");
+}else if(marks >= 90){
+    console.log("O");
 }
