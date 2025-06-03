0bb8c68224ba7d896b5e42a2dffbfa0c3fb12883
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-03 22:01:12 +0530

    practice qs solve

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-//Traffic Light System
-
-let color = "red";
-
-if(color === "red"){
-    console.log("stop");
-}if(color ==="yellow"){
-    console.log("slow down");
-}
-if(color === "green"){
-    console.log("go");
+let size = "xl";
+if(size === "xl"){
+    console.log("price is rs. 250");
+}else if(size ==="l"){
+    console.log("price is rs. 200");
+}else if(size === "m"){
+    console.log("price is rs. 100");
+    
+}else{
+    console.log("price is rs. 50");
 }
