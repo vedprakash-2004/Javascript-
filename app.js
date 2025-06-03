d78b4852de7f13808176154598a8de77c3048c32
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-03 22:14:51 +0530

    logical operator

--- app.js
+++ app.js
@@ -1,26 +1,6 @@
-// let size = "xl";
-// if(size === "xl"){
-//     console.log("price is rs. 250");
-// }else if(size ==="l"){
-//     console.log("price is rs. 200");
-// }else if(size === "m"){
-//     console.log("price is rs. 100");
-    
-// }else{
-//     console.log("price is rs. 50");
-// }
+let marks = 85;
 
-let marks = 45;
-
-if(marks >= 33){
+if(marks >= 33 && marks >= 80){
     console.log("pass");
-}else if(marks >=50){
-    console.log("b+");
-
-}else if(marks >= 70){
-    console.log("A");
-}else if(marks >= 80){
     console.log("A+");
-}else if(marks >= 90){
-    console.log("O");
 }
