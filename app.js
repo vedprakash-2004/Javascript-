febbff6f8765b69d159f9795f273c06ca47d1eaa
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-03 22:35:56 +0530

    practice qs solve

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-let str = "apple";
-
-if (str[0] ==="a" && str.length > 3){
-    console.log("good string");
-}else {
-    console.log("not a good string");
+let num = 12;
+if((num%3 ===0) && ((num+1 == 15) || (num-1 == 11))){
+    console.log("safe");
+}else{
+    console.log("unsafe");
 }
