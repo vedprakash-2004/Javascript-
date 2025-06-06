b131344e1ab45a6cbc0829258691e32d4d892c79
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-07 00:07:38 +0530

    object literals

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
-let todo = [];
+let student = {
+    name: "shradha",
+    age: 23,
+    marks: 94.4
+};
 
-let req = prompt("please enter your choice");
-
-while(true){
-    if (req == "quit"){
-        console.log("qutting app");
-        break;
-    }
-
-    if(req == "list"){
-        console.log("-------------");
-        for(task of todo){
-            console.log(task);
-        }
-        console.log("-------------");
-
-    }else if (req == "add"){
-        let task = prompt("please enter the task you want to add");
-        todo.push(task);
-        console.log("task added");
-    }else if (req == "delete"){
-        let idx = prompt ("please enter the task index");
-        todo.push(idx, 1);
-        console.log("task added");
-    }
-
-    req = prompt("please enter your request");
-}
+const item = {
+    price: 100.99,
+    discount: 50,
+    colors:["red","pink"]
+};
