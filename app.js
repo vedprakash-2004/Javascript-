d133f9346b5c8324cd8be695a4760b549150c443
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-06 23:50:07 +0530

    Todo App Only js

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-let heroes = [["superman","batman","wonder woman"], ["spiderman", "ironman", "thor"]];
+let todo = [];
 
-for(list of heroes){
-    for(hero of list){
-        console.log(hero);
+let req = prompt("please enter your choice");
+
+while(true){
+    if (req == "quit"){
+        console.log("qutting app");
+        break;
     }
+
+    if(req == "list"){
+        console.log("-------------");
+        for(task of todo){
+            console.log(task);
+        }
+        console.log("-------------");
+
+    }else if (req == "add"){
+        let task = prompt("please enter the task you want to add");
+        todo.push(task);
+        console.log("task added");
+    }else if (req == "delete"){
+        let idx = prompt ("please enter the task index");
+        todo.push(idx, 1);
+        console.log("task added");
+    }
+
+    req = prompt("please enter your request");
 }
