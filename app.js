69fc57143fa96ef86af9af20db23c8a74627bab4
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-07 00:27:10 +0530

    Add_Update

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-// const post = {
-//     username: "@vedpraksh",
-//     content: "This is my #firstPost",
-//     likesf: 150,
-//     reposts:5,
-//     tags: ["@apnacollege","@delta"]
-// };
-
-const obj = {
-    1: "a",
-    2: "b",
-    3: "c",
-    4: "d",
-    5: "e"
+const student ={
+    name: "vedprakash",
+    age: 21,
+    marks: 94.4,
+    city: "Delhi"
 };
