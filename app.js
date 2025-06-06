c61f9a636858d94ab4ca18475ee7fbd164e3b7f0
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-07 00:13:21 +0530

    creating a post

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-let student = {
-    name: "shradha",
-    age: 23,
-    marks: 94.4
-};
-
-const item = {
-    price: 100.99,
-    discount: 50,
-    colors:["red","pink"]
+const post = {
+    username: "@vedpraksh",
+    content: "This is my #firstPost",
+    likesf: 150,
+    reposts:5,
+    tags: ["@apnacollege","@delta"]
 };
