bfb927670618dc13e72029275babfcda3a2cb096
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-07 00:20:48 +0530

    conversion get value

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const post = {
-    username: "@vedpraksh",
-    content: "This is my #firstPost",
-    likesf: 150,
-    reposts:5,
-    tags: ["@apnacollege","@delta"]
+// const post = {
+//     username: "@vedpraksh",
+//     content: "This is my #firstPost",
+//     likesf: 150,
+//     reposts:5,
+//     tags: ["@apnacollege","@delta"]
+// };
+
+const obj = {
+    1: "a",
+    2: "b",
+    3: "c",
+    4: "d",
+    5: "e"
 };
