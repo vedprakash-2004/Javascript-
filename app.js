8bcaf999c9c2ce0c643ea590142b5bcda399dbe5
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-08 01:55:22 +0530

    function wiht arguments

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-function printPoem() {
-    console.log("Twinkle, twinkle, little star,");
-    console.log("How I wonder what you are!");
-    console.log(" Up above the world so high");
-    console.log("Like a diamond in the sky.");
-
-
-} 
-
-function rollDice() {
-  let rand = Math.floor(Math.random() * 6) + 1;
-  console.log(rand);
+function printInfo(name, age){
+    console.log(`${name}'s age is ${age}.`);
 }
-
-rollDice();
-rollDice();
-rollDice();
-rollDice();
-rollDice();
-rollDice();
-rollDice();
+printInfo("shradha", 23);
