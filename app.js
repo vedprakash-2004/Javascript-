e129071e258aa64a9d602acddf70582e80e14e51
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-11 01:08:52 +0530

    higher order functions

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-function multipleGreet (func, cout){
-    for(let i=1; i<=count ; i++){
-        func();
+function oddEvenTest(request){
+    if(request == "odd"){
+        return function (n){
+            console.log(!(n%2 == 0));
+        }
+
+    }else if(request == "even"){
+        return function (n){
+            console.log(n%2 == 0);
+        }
+    }else {
+        console.log("wrong request");
     }
-} 
-let greet = function() {
-    console.log("hello");
 }
-multipleGreet(greet(), 10);
+let request = "odd";
