4b9976bd599abfd524c6034502ee8a20752e1cdb
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-13 21:19:03 +0530

    practice problem solve

--- app.js
+++ app.js
@@ -1,24 +1,2 @@
-const student = {
-    name: "somu",
-    marks: 95,
-    prop: this, //global scope
-    getName: function() {
-        console.log(this);
-        return this.name;
-    },
-    getMarks: () => {
-        console.log(this); //parent's scope -> window
-        return this.marks;
-    },
-    getInfo1: function(){
-        setTimeout (() => {
-            console.log(this); //student
-        },2000);
-        
-    },
-    getInfo2: function() {
-        setTimeout( function() {
-            console.log(this); //window
-        }, 2000);
-    },
-};
+const square = (n) => n * n;
+console.log(square (4));
