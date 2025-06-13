996113e7c12f3c52f4fa6f01d2de31b78a1cbf98
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-13 21:38:53 +0530

    practice qs solve

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-const square = (n) => n * n;
-console.log(square (4));
+let id = setInterval(()=>{
+    console.log("Hello World");
+}, 2000);
+
+setTimeout(() => {
+    clearInterval(id);
+}, 10000);
