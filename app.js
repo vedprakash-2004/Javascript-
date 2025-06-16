c437c14d4a09c462e85b48f148d82e144e3c1288
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-16 22:28:11 +0530

    Maximum in Array 2nd type

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 
 let arr = [1, 4, 2, 5, 6, 7, 2, 9, 2];
 
-let max = -1;
-
-for (let i = 0; i < arr.length; i++){
-    if (max < arr[i]){
-        max = arr[i];
+let max =  arr.reduce((max, el) =>{
+    if(max < el){
+        return el;
+    }else {
+        return max;
     }
-}
+});
+
 console.log(max);
+
+
