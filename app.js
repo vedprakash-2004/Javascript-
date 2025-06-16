cd1ed6f5e1d2037c9b1efc4c4fd4b8d301ddff0d
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-17 00:23:35 +0530

    Default Parameters

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-function getMin(nums) {
-    let min = nums.reduce((min, el) => {
-    if (min < el){
-        return min;
-    }else {
-        return el;
-    }
-});
-return min ;
-
+function sum(a, b=2){
+    return a+b;
 }
-
-let nums = [10, 20, 30, 40, 5];
-
-
