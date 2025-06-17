9fd26bcc1ee1ea13c6a5504d666bc5f5464690ab
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-17 23:53:46 +0530

    Spread Object

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-let arr = [1, 2, 3, 4, 5];
-let newArr = [...arr];
-let chars = [..."hello"];
-
-
-
-let odd = [1,3, 5, 7, 9];
-let even = [2, 4, 6, 8,10];
-
-let nums = [...odd, ...even];
+const data = {
+    email: "ironman@gmailcom",
+    password:"abc",
+};
+const dataCopy = {...data}
