d61f32274c163c4ed6af0f5949807d1ed060a258
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-17 23:58:13 +0530

    spread object

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ const data = {
     email: "ironman@gmailcom",
     password:"abc",
 };
-const dataCopy = {...data}
+const dataCopy = {...data, id: 123, country:"i=India"};
+
+let arr = [1,2,3,4,5];
+let obj1 = {...arr};
+
+let obj2 = {..."hello"};
