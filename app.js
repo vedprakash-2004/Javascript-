b4e6fc283588d688382865427762365b2cfcd839
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-17 23:49:51 +0530

    Spread Array

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-function sum(a, b=2){
-    return a+b;
-}
+let arr = [1, 2, 3, 4, 5];
+let newArr = [...arr];
+let chars = [..."hello"];
+
+
+
+let odd = [1,3, 5, 7, 9];
+let even = [2, 4, 6, 8,10];
+
+let nums = [...odd, ...even];
