1a5a97dced28e8e9d930af103817fc926ff01197
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-18 00:26:44 +0530

    Reset function

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-function sum (...args){
-    return args.reduce((sum, el) => sum+el);
+function sum(...args){
+    return args.reduce((sum, el)=> sum+el);
+}
+
+function min(...args){
+    console.log(msg);
+    return args.reduce((min, el) =>{
+        if(min > el){
+            return el;
+        }else{
+          return min;
+        }
+    });
 }
