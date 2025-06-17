0d9cb14ec4a47a42ee35f2ed81ff3d3d4b4e4f4f
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-18 00:18:08 +0530

    Reset function use

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
 function sum (...args){
-    for(let i=0; i< args.length; i++){
-        console.log("you gave us:", args[i]);
-    }
-}
-
-function main (a, b, c, d){
-    console.log(arguments);
+    return args.reduce((sum, el) => sum+el);
 }
