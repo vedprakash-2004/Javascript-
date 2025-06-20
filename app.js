fb117bec16299fd9882a19f908720ccc92d4a336
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-20 07:49:47 +0530

    practice question solve

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ para1.innerText = "Hey I'm red!";
 document.querySelector("body").append(para1); 
 
 para1.classList.add('red');
+
+
+let h3 = document.createElement("h3");
+h3.innerText = "I'm a blue h3";
+document.querySelector("body").append(h3);
+
+h3.classList.add("blue");
