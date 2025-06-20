e34a06cfb1ff921d172ede6b484edba82b74ff99
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-20 07:45:49 +0530

    practice qs solve dom fuction

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let para1 = document.createElement('p');
-para1.innerText = "Hey I'm red";
-document.querySelector('body').append(para1);
-pard1.classList.add("red");
+para1.innerText = "Hey I'm red!";
+document.querySelector("body").append(para1); 
+
+para1.classList.add('red');
