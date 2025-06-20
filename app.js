58df558161866d2c3e22bcd3eb6eac6bbb7093fc
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-20 08:00:02 +0530

    practice qs solve

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ h3.innerText = "I'm a blue h3";
 document.querySelector("body").append(h3);
 
 h3.classList.add("blue");
+
+
+let div = document.createElement("div");
+let h1 = document.createElement("h1");
+let para2 = document.createElement("p");
+
+h1.innerText = "I'm in a div";
+para2.innerText = "ME TOO!";
+
+div.append(h1);
+div.append(para2);
+div.classList.add("box");
+document.querySelector("body").append(div);
