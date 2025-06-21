b9a83b98484e3a8a6233381a13fff22322ae1c91
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-21 23:58:15 +0530

    learn Mouse_Pointer Events

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ let btn = document.querySelector("button");
 
 for (btn of btns){
     btn.oneclick = sayHello;
+    btn.onmouseenter = function(){
+        console.log("you entered a button");
+    }
+    console.dir(btn);
 
 }
 function sayHello() {
-    alert("Hello");
+    alert("Hello!");
 }
