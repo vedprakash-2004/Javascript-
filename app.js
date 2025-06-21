dd6afd25906dae4780a87f28cbbeb55994cb2243
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-22 00:16:32 +0530

    learn Event Listeners

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-let btn = document.querySelector("button");
+let btns = document.querySelectorAll("button");
 
-for (btn of btns){
-    btn.oneclick = sayHello;
-    btn.onmouseenter = function(){
-        console.log("you entered a button");
-    }
-    console.dir(btn);
+for (btn of btns) {
+//     btn.onclick = sayHello;
+//     btn.onclick = sayHello;
+
+btn.addEventListener("click", sayHello);
+btn.addEventListener("click", sayName);
 
 }
-function sayHello() {
+
+function sayHello () {
     alert("Hello!");
+}
+
+function sayHello() {
+    alert("Apna college");
 }
