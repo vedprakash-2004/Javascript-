228f54334266f9c195fbe459d1dfe5105b13c76b
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-24 02:08:51 +0530

    learn keyboard events

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let btn = document.querySelector("button");
 
-btn.addEventListener("click", function(){
+btn.addEventListener("click", function(event){
+    console.log(event);
      console.log("button clicked");
 });
    
