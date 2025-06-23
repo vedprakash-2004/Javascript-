44aa368302997e4e73d8ca80e15932875a2931cc
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-24 02:07:07 +0530

    Learn Keyboard Events

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 let btn = document.querySelector("button");
-let p = document.querySelector("p");
-let h1 = document.querySelector("h1");
-let h3= document.querySelector("h3");
 
+btn.addEventListener("click", function(){
+     console.log("button clicked");
+});
+   
 
-function changeColor() {
-    console.dir(this.innerText);
-    this.style.bacgroundColor = "blue";
-}
-
-btn.addEventListener("click", changeColor);
-p.addEventListener("click", changeColor);
-h1.addEventListener("click",changeColor);
-h3.addEventListener("click", changeColor);
 
 
