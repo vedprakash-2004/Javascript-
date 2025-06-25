f30f1aab346d2de15ef9f854b2dedf6b2b2e036b
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-25 19:23:29 +0530

    learn keyboard events

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let inp = document.querySelector("input");
 
-// inp.addEventListener("keydown", function()){
-//     console.log ("key was pressed");
-// };
-
-inp.addEventListener("keyup", function(){
-    console.log ("key was released");
+inp.addEventListener("keydown", function(){
+    console.log ("key was pressed");
 });
+
+// inp.addEventListener("keyup", function(){
+//     console.log ("key was released");
+// });
