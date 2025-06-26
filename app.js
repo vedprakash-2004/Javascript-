de3ae1335cf6e42c8016c75dc92ef774c75ba60e
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-26 23:50:44 +0530

    learn keyboard Event

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let inp = document.querySelector("input");
 
 inp.addEventListener("keydown", function(event){
     console.log(event.key);
-    console.log(event.key);
+    console.log("conde =", event.code);
     console.log("key was pressed");
 });
    
