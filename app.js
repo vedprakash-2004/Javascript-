9a01576fe4fcf8b8a96c5d38092938a4f36fc023
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-27 01:25:09 +0530

    learn keyboard Events

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ let inp = document.querySelector("input");
 
 inp.addEventListener("keydown", function(event){
   
-   console.log("code =", event.code);
-   if (event.code == "ArrowUp"){
+   console.log("code =", event.code); //ArrowUp(U), ArrowDown(D), ArrowLeft(L), ArrowRight(R)
+   if (event.code == "keyU"){
      console.log("character moves forward");
-   } else if(event.code == "ArrowUp"){
+   } else if(event.code == "keyD"){
      console.log("character moves backward");
-   } else if(event.code == "ArrowLeft"){
+   } else if(event.code == "keyL"){
     console.log("character moves left");
-   } else if (event.code == "ArrowRight"){
+   } else if (event.code == "keyR"){
     console.log("character moves right");
    }
 });
