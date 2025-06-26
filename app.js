f2d1d146818a2e40f29c3e53d4c2ac02a0f9cd07
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-27 01:35:07 +0530

    learn Form Events

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
-let inp = document.querySelector("input");
+let form = document.querySelector("form");
 
-inp.addEventListener("keydown", function(event){
-  
-   console.log("code =", event.code); //ArrowUp(U), ArrowDown(D), ArrowLeft(L), ArrowRight(R)
-   if (event.code == "keyU"){
-     console.log("character moves forward");
-   } else if(event.code == "keyD"){
-     console.log("character moves backward");
-   } else if(event.code == "keyL"){
-    console.log("character moves left");
-   } else if (event.code == "keyR"){
-    console.log("character moves right");
-   }
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    alert("form submitted");
 });
-   
