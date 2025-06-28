a50c0da8b61ced9f8e330bbceac7360fdfb49b7f
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-06-29 00:28:56 +0530

    learn next step in extracting Form Data

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let form = document.querySelector("form");
 
+
+
 form.addEventListener("submit", function(event){
     event.preventDefault();
+    console.dir(form);
+
     
-    let user = document.querySelector("#user");
-    let pass = document.querySelector("#pass");
+    let user = this.elements[0];
+    let pass = this.elements[1];
+
     console.log(user.value);
     console.log(pass.value);
 
