caa7658eeb981e008828cf76c81e2f3f7ed56986
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-01 17:27:47 +0530

    learn js call stack

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-let inp = document.querySelector("#text");
-let p = document.querySelector("p");
-
-inp.addEventListener("input", function(){
-    console.log(inp.value);
-    p.innerText = inp.value;
-});
+function hello() {
+    console.log("hello");
+    
+}
+hello();
