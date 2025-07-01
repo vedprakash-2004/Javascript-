bce70991bbb48fa7b19a64872873eb8887ff6bfe
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-01 17:32:38 +0530

    learn next step js call stack

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 function hello() {
+    console.log("inside hello fnx");
     console.log("hello");
     
 }
-hello();
+
+function demo(){
+    console.log("calling hello fnx");
+    hello();
+
+}
+console.log("calling demo fnx");
+demo();
