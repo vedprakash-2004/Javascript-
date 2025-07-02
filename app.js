292130830a04300e7b072d387c821a58e0b5ce05
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-03 03:36:47 +0530

    learn callback hell

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-// let a = 25;
-// console.log(a);
-// let b = 10;
-// console.log(b);
-// console.log(a+b);
+h1 = document.querySelector("h1");
 
-setTimeout (() => {
-    console.log("apna college");
-
-} ,2000);
-
-setTimeout (() => {
-    console.log("apna college");
-
-} ,2000);
-console.log("hello....");
+setTimeout(() => {
+   h1.style.color ="red";
+}, 1000);
