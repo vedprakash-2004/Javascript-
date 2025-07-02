5a0c772b27f6114bd3c1626a09a16f1c795c137e
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-03 03:59:13 +0530

    Learn Callback Hell

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 h1 = document.querySelector("h1");
 
-setTimeout(() => {
-   h1.style.color ="red";
-}, 1000);
+function changeColor(color, delay, nextColorChange){
+    setTimeout(()=> {
+        h1.style.color = color;
+        if (nextColorChange) nextColorChange();
+        nextColorChange = color;
+    }, delay);
+}
+
+changeColor("red", 1000, () => {
+    changeColor("orange", 1000, () => {
+        changeColor("green", 1000, () => {
+            changeColor("yellow", 1000, () => {
+                changeColor("blue", 1000, () => {
+
+                })
+
+            })
+
+
+         });
+    
+    });
+   
+});
+
