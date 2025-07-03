7a17da1a12f6d763a2b80b371b4a60f4b65a6b96
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-04 01:32:01 +0530

    Learn next step Setting up for promises

--- app.js
+++ app.js
@@ -26,12 +26,22 @@
 // });
 
 
-function savetoDb(data){
+function savetoDb(data, success, failure){
     let internetSpeed = Math.floor(Math.random() *10)+1;
     if(internetSpeed > 4){
-        console.log("your data was saved:", data);
+        success();
     }else {
-        console.log("weak connection. data not save");
+        failure(); 
     }
 }
+
+savetoDb(
+    "apna college",
+    () => {
+        console.log("your data was saved : ");
+    },
+    () => {
+        console.log("week connection. data not saved");
+    }
+);
 
