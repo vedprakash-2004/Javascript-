6f8d6faa59350139f1a5b04eb4cbf554c7bd1422
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-04 01:23:40 +0530

    setting up for promises

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-h1 = document.querySelector("h1");
+// h1 = document.querySelector("h1");
 
-function changeColor(color, delay, nextColorChange){
-    setTimeout(()=> {
-        h1.style.color = color;
-        if (nextColorChange) nextColorChange();
-        nextColorChange = color;
-    }, delay);
-}
+// function changeColor(color, delay, nextColorChange){
+//     setTimeout(()=> {
+//         h1.style.color = color;
+//         if (nextColorChange) nextColorChange();
+//         nextColorChange = color;
+//     }, delay);
+// }
 
-changeColor("red", 1000, () => {
-    changeColor("orange", 1000, () => {
-        changeColor("green", 1000, () => {
-            changeColor("yellow", 1000, () => {
-                changeColor("blue", 1000, () => {
+// changeColor("red", 1000, () => {
+//     changeColor("orange", 1000, () => {
+//         changeColor("green", 1000, () => {
+//             changeColor("yellow", 1000, () => {
+//                 changeColor("blue", 1000, () => {
 
-                })
+//                 })
 
-            })
+//             })
 
 
-         });
+//          });
     
-    });
+//     });
    
-});
+// });
+
+
+function savetoDb(data){
+    let internetSpeed = Math.floor(Math.random() *10)+1;
+    if(internetSpeed > 4){
+        console.log("your data was saved:", data);
+    }else {
+        console.log("weak connection. data not save");
+    }
+}
 
