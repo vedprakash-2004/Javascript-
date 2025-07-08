9b264368ec0a8fbe16926a6fda089630b6d3efd2
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-08 09:33:06 +0530

    learn refactoring old code

--- app.js
+++ app.js
@@ -7,9 +7,26 @@ function changeColor(color, delay){
       resolve("color changed");
         
     }, delay);
-  })
+  }); 
 
 }
+
+changeColor("red", 1000)
+.then(()=> {
+  console.log("red color was completed");
+  return changeColor("orange",1000);
+})
+.then(() =>{
+  console.log("orange color was completed");
+  return changeColor("green", 1000);
+})
+.then(() =>{
+  console.log("green color was completed");
+  return changeColor("blue", 1000);
+})
+.then(()=>{
+  console.log("blue color was completed");
+});
 
 // changeColor("red", 1000, () => {
 //     changeColor("orange", 1000, () => {
