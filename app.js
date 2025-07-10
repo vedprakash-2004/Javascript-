e5fab19560350bf162fc0f4a3a77e9365ff4c008
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-11 01:15:53 +0530

    learn next step await keyword

--- app.js
+++ app.js
@@ -50,9 +50,19 @@
 
 
 function getNum() {
-  console.log(5);
+  return new Promise((resolve, reject)=> {
+    setTimeout(() => {
+       let num = Math.floor (Math.random() *10)+1;
+       console.log(num);
+       resolve();
+    }, 1000);
+  
+
+  });
 }
 
-async function demo() {
-  getNum();
+async function  demo() {
+ getNum();
+ getNum();
+ getNum();
 }
