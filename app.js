d4ca6f9e7c03d1467dbc978b980505b8e040609a
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-11 01:06:09 +0530

    learn await keyword

--- app.js
+++ app.js
@@ -29,21 +29,30 @@
 // });
 
 
-function greet(){
-  throw "weak connection";
-  return "Hello!";
+// function greet(){
+//   throw "weak connection";
+//   return "Hello!";
+// }
+
+// greet()
+// .then((result)=>{
+//   console.log("promise was resolved");
+//   console.log("result was :", result);
+
+// })
+// .catch((err)=>{
+//   console.log("promise was rejected with err: ", err);
+// });
+
+// let demo = async () => {
+//   return 5;
+// };
+
+
+function getNum() {
+  console.log(5);
 }
 
-greet()
-.then((result)=>{
-  console.log("promise was resolved");
-  console.log("result was :", result);
-
-})
-.catch((err)=>{
-  console.log("promise was rejected with err: ", err);
-});
-
-let demo = async () => {
-  return 5;
-};
+async function demo() {
+  getNum();
+}
