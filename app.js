d7bf75ef8c6194d001c84f9f0598100407f0f8d6
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-12 01:00:24 +0530

    learn Handing Rejections

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ h1 = document.querySelector("h1");
 function changeColor(color, delay){
   new Promise((resolve, reject)=>{
       setTimeout(()=> {
+        let num = Math.floor(Math.random() * 5) + 1;
+        if(num > 3){
+          reject("promise rejected");
+        }
+        
       h1.style.color = color;
+      console.log(`color changed to ${color}`);
       resolve("color changed");
         
     }, delay);
@@ -22,58 +28,3 @@ async function demo() {
   console.log("new number = ", a+3);
 }
 
-// changeColor("red", 1000)
-// .then(()=> {
-//   console.log("red color was completed");
-//   return changeColor("orange",1000);
-// })
-// .then(() =>{
-//   console.log("orange color was completed");
-//   return changeColor("green", 1000);
-// })
-// .then(() =>{
-//   console.log("green color was completed");
-//   return changeColor("blue", 1000);
-// })
-// .then(()=>{
-//   console.log("blue color was completed");
-// });
-
-
-// function greet(){
-//   throw "weak connection";
-//   return "Hello!";
-// }
-
-// greet()
-// .then((result)=>{
-//   console.log("promise was resolved");
-//   console.log("result was :", result);
-
-// })
-// .catch((err)=>{
-//   console.log("promise was rejected with err: ", err);
-// });
-
-// let demo = async () => {
-//   return 5;
-// };
-
-
-// function getNum() {
-//   return new Promise((resolve, reject)=> {
-//     setTimeout(() => {
-//        let num = Math.floor (Math.random() *10)+1;
-//        console.log(num);
-//        resolve();
-//     }, 1000);
-  
-
-//   });
-// }
-
-// async function  demo() {
-// await getNum();
-//  await getNum();
-//  getNum();
-// }
