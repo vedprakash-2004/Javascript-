ed530ce1bcad2c4c34abd85ae601412c87fce1e4
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-13 23:56:50 +0530

    our first API request

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let url = "https://catfact.ninja/fact";
 fetch(url)
 .then((res) => {
   console.log(res);
+  res.json().then((data) => {
+    console.log(data);
+  });
 })
 .catch((err) => {
   console.log("ERROR-",err);
