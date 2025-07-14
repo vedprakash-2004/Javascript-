fc665cfc80221d66623f3aeb8973ce6f7e04027a
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-14 13:47:07 +0530

    Using Axios

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
+let btn = document.querySelector("button");
+btn.addEventListener("click", () =>{
+  console.log("button was clicked");
+});
+
 let url = "https://catfact.ninja/fact";
 
-async function getFacts() {
-  try {
-    let res = await fetch (url);
-    let data = await res.json();
-    console.log(data.fact);
-
-    let res2 = await fetch(url);
-    let data2 = await res2.json();
-    console.log(data2.fact);
-  } catch (e) {
-    console.log("error - ", e);
+async function getFacts(){
+  try{
+    let res = await axios.get(url);
+    console.log(res.data.facts);
+  }catch (e){
+    console.log("error -", e);
   }
-  console.log("bye");
-
 }
+
