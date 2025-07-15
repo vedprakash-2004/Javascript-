65d9e9ba6b7b5cbc28592f63ab14d0840abc7b0e
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-15 16:02:50 +0530

    Dog pictures API

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 let btn = document.querySelector("button");
+let url2 ="https://dog.ceo/api/breeds/image/random";
+
+
+
 btn.addEventListener("click", async () =>{
-  let fact = await getFacts();
-  console.log( fact);
-  let p = document.querySelector("#result");
-  p.innerText = fact;
+  // let fact = await getFacts();
+  // // console.log( fact);
+  // let p = document.querySelector("#result");
+  // p.innerText = fact;
 });
 
-let url = "https://catfact.ninja/fact";
-
-async function getFacts(){
+async function getImage(){
   try{
-    let res = await axios.get(url);
-    console.log(res.data.facts);
+    let res = await axios.get(url2);
+    console.log(res.data.message);
   }catch (e){
     console.log("error -", e);
+    return "No Image found ";
   }
 }
+
+// let url = "https://catfact.ninja/fact";
+
+
+// async function getFacts(){
+//   try{
+//     let res = await axios.get(url);
+//     return res.data.fact;
+//   }catch (e){
+//     console.log("error -", e);
+//     return "No fact found ";
+//   }
+// }
 
