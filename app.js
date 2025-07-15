9888b062e23abebca849d8b7cc30a4914520c2b9
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-15 16:13:04 +0530

    DogPictures API next step

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ let url2 ="https://dog.ceo/api/breeds/image/random";
 
 
 btn.addEventListener("click", async () =>{
-  // let fact = await getFacts();
-  // // console.log( fact);
-  // let p = document.querySelector("#result");
-  // p.innerText = fact;
+ let link = await getImage();
+//  console.log(link);
+let image = document.querySelector("#result");
+img.setAttribute("src",link);
 });
 
 async function getImage(){
   try{
     let res = await axios.get(url2);
-    console.log(res.data.message);
+   return res.data.message;
   }catch (e){
     console.log("error -", e);
-    return "No Image found ";
+    return "/ ";
   }
 }
 
