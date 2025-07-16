2f89181c7ff255ffa2b4b0762cf599d283eb1e68
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-16 15:01:47 +0530

    sending Headers with API request

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
-let btn = document.querySelector("button");
-let url2 ="https://dog.ceo/api/breeds/image/random";
+consturl = "https://echo.hoppscotch.io";
 
-
-
-btn.addEventListener("click", async () =>{
- let link = await getImage();
-//  console.log(link);
-let image = document.querySelector("#result");
-img.setAttribute("src",link);
-console.log(link);
-});
-
-async function getImage(){
+async function getJokes() {
   try{
-    let res = await axios.get(url2);
-   return res.data.message;
-  }catch (e){
-    console.log("error -", e);
-    return "/ ";
+    let res = await axios.get(url);
+    console.log(res);
+  }catch (err){
+    console.log(err);
   }
+  
+
 }
-
-// let url = "https://catfact.ninja/fact";
-
-
-// async function getFacts(){
-//   try{
-//     let res = await axios.get(url);
-//     return res.data.fact;
-//   }catch (e){
-//     console.log("error -", e);
-//     return "No fact found ";
-//   }
-// }
-
